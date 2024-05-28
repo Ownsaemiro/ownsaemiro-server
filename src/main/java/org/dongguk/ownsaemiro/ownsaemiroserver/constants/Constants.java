02cfc0400849830c33d5a7251385f57880393024
Lim jeong woo <dlawjddn0105@gmail.com>
2024-05-28 22:42:42 +0900

    Chore: 오타 수정

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/constants/Constants.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static String CLAIM_USER_ID = "uuid";
     public static String CLAIM_USER_ROLE = "role";
     public static String PREFIX_BEARER = "Bearer ";
-    public static String PREFIX_AUTH = "authorization";
+    public static String PREFIX_AUTH = "Authorization";
     public static String ACCESS_COOKIE_NAME = "access_token";
     public static String REFRESH_COOKIE_NAME = "refresh_token";
     public static List<String> NO_NEED_AUTH = List.of(
