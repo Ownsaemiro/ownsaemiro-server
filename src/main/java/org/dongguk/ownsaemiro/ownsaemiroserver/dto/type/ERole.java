15fe84fc527cfd24217636448a305edd7c0b23cd
Lim jeong woo <dlawjddn0105@gmail.com>
2024-05-28 22:44:03 +0900

    Feat: ERole matcher 생성

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/type/ERole.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/type/ERole.java
@@ -2,14 +2,24 @@ package org.dongguk.ownsaemiro.ownsaemiroserver.dto.type;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.dongguk.ownsaemiro.ownsaemiroserver.exception.CommonException;
+import org.dongguk.ownsaemiro.ownsaemiroserver.exception.ErrorCode;
 
 @Getter
 @RequiredArgsConstructor
 public enum ERole {
+    GUEST("GUEST", "ROLE_GUEST"),
     USER("USER", "ROLE_USER"),
-    SELLER("SELLR", "ROLE_SELLER"),
+    SELLER("SELLER", "ROLE_SELLER"),
     ADMIN("ADMIN", "ROLE_ADMIN");
     private final String role;
     private final String securityRole;
+    public static ERole toERole(String strRole){
+        for(ERole eRole : ERole.values()){
+            if (eRole.role.equals(strRole))
+                return eRole;
+        }
+        throw new CommonException(ErrorCode.INVALID_PARAMETER_FORMAT);
+    }
 
 }
