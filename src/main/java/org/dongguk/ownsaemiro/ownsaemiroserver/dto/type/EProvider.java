27cb4b13c8dbc106dad0c9fcaa91ec58c6654c7b
Lim jeong woo <dlawjddn0105@gmail.com>
2024-05-28 22:43:24 +0900

    Feat: Default provider 추가

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/type/EProvider.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/type/EProvider.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum EProvider {
+    DEFAULT("DEFAULT"),
     KAKAO("KAKAO"),
     NAVER("NAVER");
     private final String provider;
