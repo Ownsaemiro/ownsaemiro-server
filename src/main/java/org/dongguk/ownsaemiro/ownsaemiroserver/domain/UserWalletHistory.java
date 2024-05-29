cbe099101df545c7747938482a08939f62f9834f
Lim jeong woo <dlawjddn0105@gmail.com>
2024-05-29 16:36:00 +0900

    Feat: user wallet domain 생성

---
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/domain/UserWalletHistory.java
@@ -0,0 +1,32 @@
+package org.dongguk.ownsaemiro.ownsaemiroserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Table(name = "user_wallet_histories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserWalletHistory {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*  사용자 지갑 구매 내역 기본 속성  */
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDate createdAt;
+
+    /*  연관 관계 속성  */
+    @ManyToOne
+    @JoinColumn(name = "user_wallet_id", nullable = false)
+    private UserWallet userWallet;
+
+}
