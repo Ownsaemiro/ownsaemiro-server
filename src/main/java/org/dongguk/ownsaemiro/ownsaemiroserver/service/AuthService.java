82e115816162424784cc917b073fba1f861985da
Lim jeong woo <dlawjddn0105@gmail.com>
2024-05-29 16:38:05 +0900

    Chore: 사용자 생성시, 사용자 지갑 생성 todo 추가

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/service/AuthService.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/service/AuthService.java
@@ -8,6 +8,7 @@ import org.dongguk.ownsaemiro.ownsaemiroserver.dto.type.ERole;
 import org.dongguk.ownsaemiro.ownsaemiroserver.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -15,9 +16,11 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    @Transactional
     public void signUp(TestAuthSignUpDto authSignUpDto){
         userRepository.save(
                 User.signUp(authSignUpDto, passwordEncoder.encode(authSignUpDto.password()), ERole.toERole(authSignUpDto.role()))
         );
+        //TODO: 사용자 지갑 생성 로직 필요
     }
 }
