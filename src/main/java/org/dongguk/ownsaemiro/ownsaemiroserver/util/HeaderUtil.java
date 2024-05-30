e192ae688123921083f63f22f5de14f1bf9305ed
Lim jeong woo <dlawjddn0105@gmail.com>
2024-05-30 11:15:18 +0900

    Feat: 사용자 생성 유틸 함수 생성 및 추가

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/util/HeaderUtil.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/util/HeaderUtil.java
@@ -1,6 +1,9 @@
 package org.dongguk.ownsaemiro.ownsaemiroserver.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.dongguk.ownsaemiro.ownsaemiroserver.constants.Constants;
+import org.dongguk.ownsaemiro.ownsaemiroserver.dto.response.JwtTokenDto;
 import org.dongguk.ownsaemiro.ownsaemiroserver.exception.CommonException;
 import org.dongguk.ownsaemiro.ownsaemiroserver.exception.ErrorCode;
 import org.springframework.util.StringUtils;
@@ -13,5 +16,14 @@ public class HeaderUtil {
         if (!StringUtils.hasText(headerValue) || !headerValue.startsWith(prefix))
             throw new CommonException(ErrorCode.INVALID_HEADER_VALUE);
         return Optional.of(headerValue.substring(prefix.length()));
+    }
+
+    public static void addHeader(HttpServletResponse response, String headerName, String value){
+        response.addHeader(headerName, value);
+    }
+
+    public static void addHeaders(HttpServletResponse response, JwtTokenDto jwtTokenDto){
+        addHeader(response, Constants.ACCESS_COOKIE_NAME,jwtTokenDto.accessToken());
+        addHeader(response, Constants.REFRESH_COOKIE_NAME,jwtTokenDto.refreshToken());
     }
 }
--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/util/RestClientUtil.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/util/RestClientUtil.java
@@ -1,12 +1,17 @@
 package org.dongguk.ownsaemiro.ownsaemiroserver.util;
 
 import net.minidev.json.JSONObject;
+import org.dongguk.ownsaemiro.ownsaemiroserver.constants.Constants;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 @Component
 public class RestClientUtil {
@@ -20,7 +25,28 @@ public class RestClientUtil {
                 .body(requestBody.toJSONString())
                 .retrieve()
                 .toEntity(Map.class).getBody());
+
         return new JSONObject(response);
+    }
+
+    public JSONObject sendAppKakaoLoginRequest(String url, String accessToken){
+        // TODO: 사용자 정보 어떻게 받을지 논의 후 수정할 예정
+        Map<String,Object> response = Objects.requireNonNull(restClient.get()
+                .uri(url)
+                .header(Constants.CONTENT_TYPE, Constants.CONTENT_VALUE)
+                .header(Constants.PREFIX_AUTH, Constants.PREFIX_BEARER + accessToken)
+                .retrieve()
+                .toEntity(Map.class).getBody());
+
+        return new JSONObject(response);
+    }
+
+    public Map<String, Object> sendAppNaverLoginRequest(String url, String accessToken){
+        return (Map<String, Object>) Objects.requireNonNull(restClient.get()
+                .uri(url)
+                .header(Constants.PREFIX_AUTH, Constants.PREFIX_BEARER + accessToken)
+                .retrieve()
+                .toEntity(Map.class).getBody().get("response"));
     }
 
     public JSONObject sendPatchRequest(String url, JSONObject requestBody) {
