73c2d21441c1a30e380e33056b37409f5b4a84ae
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-06 21:01:51 +0900

    Feat: 행사 리뷰 목록 조회 api 생성

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/EventReviewRepository.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/EventReviewRepository.java
@@ -2,10 +2,14 @@ package org.dongguk.ownsaemiro.ownsaemiroserver.repository;
 
 import org.dongguk.ownsaemiro.ownsaemiroserver.domain.Event;
 import org.dongguk.ownsaemiro.ownsaemiroserver.domain.EventReview;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EventReviewRepository extends JpaRepository<EventReview, Long> {
     List<EventReview> findTop3ByEvent(Event event);
+
+    Page<EventReview> findAllByEvent(Event event, Pageable pageable);
 }
