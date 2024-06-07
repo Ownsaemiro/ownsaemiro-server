1bc46d4ac24c4a08e01b695266f6c26731270360
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-08 03:18:06 +0900

    Fix: camel case -> snake case로 변경

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/response/JwtTokenDto.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/response/JwtTokenDto.java
@@ -1,10 +1,15 @@
 package org.dongguk.ownsaemiro.ownsaemiroserver.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.io.Serializable;
 @Builder
-public record JwtTokenDto(String accessToken, String refreshToken) implements Serializable {
+public record JwtTokenDto(
+        @JsonProperty("access_token")
+        String accessToken,
+        @JsonProperty("refresh_token")
+        String refreshToken) implements Serializable {
     public static JwtTokenDto of (String accessToken, String refreshToken){
         return JwtTokenDto.builder()
                 .accessToken(accessToken)
