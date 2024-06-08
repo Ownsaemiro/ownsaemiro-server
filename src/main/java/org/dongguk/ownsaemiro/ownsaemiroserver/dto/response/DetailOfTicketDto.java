d2cae853a62c63133c9c7300cf5c70e166498b0e
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-09 00:18:33 +0900

    ✨ [Feature] - 이벤트 판매 상태 변경 스케줄링 추가 (#67)
    * Fix: 관리자 승인 시 행사의 상태 변경 안함
    
    * Fix: 사용자 티켓 구매 내역: eventId -> ticketId로 변경

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/response/DetailOfTicketDto.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/response/DetailOfTicketDto.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 @Builder
 public record DetailOfTicketDto(
         @JsonProperty("id")
-        Long eventId,
+        Long ticketId,
 
         @JsonProperty("image")
         String image,
