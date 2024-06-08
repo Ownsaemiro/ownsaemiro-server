3df4217953c68700f25c114391ec029724d6fb27
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-09 02:41:12 +0900

    🛠️ [ Refactor] - DB Column Rollback (#77)
    * Refactor: DB Column 삭제 Rollback
    
    * Refactor: service logic 적용

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/request/WriteReviewOfEvent.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/dto/request/WriteReviewOfEvent.java
@@ -3,6 +3,9 @@ package org.dongguk.ownsaemiro.ownsaemiroserver.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record WriteReviewOfEvent(
+        @JsonProperty("title")
+        String title,
+
         @JsonProperty("content")
         String content
 ) {
