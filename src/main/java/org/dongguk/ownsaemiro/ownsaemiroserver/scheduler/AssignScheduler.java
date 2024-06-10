ea76b81d0c87883c7cbbb2cc4ab6e300db1b7b21
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-10 21:23:49 +0900

    Feat: 스케쥴러 로직 @Transactional 어노테이션 추가

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/scheduler/AssignScheduler.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/scheduler/AssignScheduler.java
@@ -5,15 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.dongguk.ownsaemiro.ownsaemiroserver.service.TicketService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AssignScheduler {
     private final TicketService ticketService;
-    @Scheduled(cron = "0 */5 * * * *")
+    @Transactional
+    @Scheduled(cron = "0 0 */1 * * *")
     public void assignTickets(){
-        ticketService.assignTickets();
-        ticketService.failToAssignTicket();
-        log.info("상태 변경 완료");
+        if (ticketService.assignTickets()){
+            ticketService.failToAssignTicket();
+            log.info("상태 변경 완료");
+        }
     }
 }
--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/scheduler/EventScheduler.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/scheduler/EventScheduler.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.dongguk.ownsaemiro.ownsaemiroserver.service.EventService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class EventScheduler {
     private final EventService eventService;
 
+    @Transactional
     @Scheduled(cron = "0 0 0 * * *")
     public void updateEventStatus(){
         eventService.updateEventState();
