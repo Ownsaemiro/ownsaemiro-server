43d921b91d37b888ffe1a8f4aa2499d2d4fcb7c2
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-10 21:28:50 +0900

    🐛 [Fix] - Fix FCM and Solving DB Update (#105)
    * Chore: @DynamicUpdate 어노테이션 추가
    
    * Fix: select 조건 없는 에러 해결
    
    * Feat: 스케쥴러 로직 @Transactional 어노테이션 추가
    
    * Feat: FCM 로직 분리, @Transactional 어노테이션 추가

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/UserAssignTicketRepository.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/UserAssignTicketRepository.java
@@ -3,16 +3,17 @@ package org.dongguk.ownsaemiro.ownsaemiroserver.repository;
 import org.dongguk.ownsaemiro.ownsaemiroserver.domain.Ticket;
 import org.dongguk.ownsaemiro.ownsaemiroserver.domain.User;
 import org.dongguk.ownsaemiro.ownsaemiroserver.domain.UserAssignTicket;
+import org.dongguk.ownsaemiro.ownsaemiroserver.dto.type.EAssignStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserAssignTicketRepository extends JpaRepository<UserAssignTicket, Long> {
-
     Boolean existsByUserAndTicket(User user, Ticket ticket);
 
     Optional<UserAssignTicket> findByUserAndTicket(User user, Ticket ticket);
@@ -25,10 +26,13 @@ public interface UserAssignTicketRepository extends JpaRepository<UserAssignTick
             "        user_assign_tickets ut " +
             "    INNER JOIN " +
             "        tickets t ON ut.ticket_id = t.id " +
-            ") AS ut " + // 테이블 별칭 변경
+            "    WHERE " +
+            "        ut.state = 'WAITING' " +
+            ") AS ut " +
             "WHERE " +
             "    rn = 1", nativeQuery = true)
     List<UserAssignTicket> findRandomUserAssignTicket();
+
 
     @Query("select ut from UserAssignTicket ut where ut.status = org.dongguk.ownsaemiro.ownsaemiroserver.dto.type.EAssignStatus.WAITING")
     List<UserAssignTicket> findFailToAssignTicket();
