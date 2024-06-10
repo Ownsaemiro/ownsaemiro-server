69fc12c159ffbbface2b3e67e369eb96f3f258d3
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-10 16:54:44 +0900

    Chore: 오타 수정

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/NotificationRepository.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/NotificationRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Page<Notification> findAllByUser(User user, Pageable pageable)
+    Page<Notification> findAllByUser(User user, Pageable pageable);
 }
