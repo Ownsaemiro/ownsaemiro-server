dfaf02839676d0e5c688abc8616feba7c691641b
Lim jeong woo <dlawjddn0105@gmail.com>
2024-06-11 21:58:11 +0900

    Fix: select 쿼리 조건 수정

--- src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/UserAssignTicketRepository.java
+++ src/main/java/org/dongguk/ownsaemiro/ownsaemiroserver/repository/UserAssignTicketRepository.java
@@ -27,7 +27,7 @@ public interface UserAssignTicketRepository extends JpaRepository<UserAssignTick
             "    INNER JOIN " +
             "        tickets t ON ut.ticket_id = t.id " +
             "    WHERE " +
-            "        ut.state = 'WAITING' " +
+            "        ut.status = 'WAITING' AND t.status = 'TRANSFER' " +
             ") AS ut " +
             "WHERE " +
             "    rn = 1", nativeQuery = true)
